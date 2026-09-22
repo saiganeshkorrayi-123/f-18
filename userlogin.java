ac6fd2c77c2341f70e39c502b1ce114163bba05b
Ganesh <ganeshkorrai@gmail.com>
2026-09-22 20:01:36 +0530

    modified file2.txt

--- userlogin.java
+++ userlogin.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class userlogin{
+public class userlogin{ 
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
           System.out.println("Enter the choice from the user:");
