8212e384e5e202567d417bdf5f18f63f25e4662a
kiran <kiran123@gmail.com>
2026-09-23 11:08:03 +0530

    AC.java GC.java vc.java file1.txt

--- Ac.java
+++
@@ -1,7 +0,0 @@
-public class Ac {
-     public static void main(String[]args){
-        System.out.println("This is a Audiocall section");
-    }
-    
-    
-}
--- GC.java
+++
@@ -1,7 +0,0 @@
-public class GC {
-     public static void main(String[]args){
-        System.out.println("This is a Groupcall section");
-    }
-    
-    
-}
